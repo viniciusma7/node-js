8cdad4e3420e80239efceead741c301db64eb428
viniciusma7 <159086837+viniciusma7@users.noreply.github.com>
2025-12-04 22:38:06 -0300

    wip

--- modules/fs.js
+++ modules/fs.js
@@ -21,27 +21,27 @@ fs.writeFile(path.join(__dirname, '/test', 'test.txt'),
   };
 
   console.log('File written...');
-});
 
-// Append to a file
-fs.appendFile(path.join(__dirname, '/test', 'test.txt'),
-  '\nI love Node.js', 
-  (err) => {
-  if (err) {
-    return console.log('Error appending to file:', err);
-  };
+  // Append to a file
+  fs.appendFile(path.join(__dirname, '/test', 'test.txt'),
+    '\nI love Node.js', 
+    (err) => {
+    if (err) {
+      return console.log('Error appending to file:', err);
+    };
 
-  console.log('File appended...');
-});
+    console.log('File appended...');
+  });
 
-// Read a file
-fs.readFile(path.join(__dirname, '/test', 'test.txt'),
-  'utf-8',
-  (err, data) => {
-  if (err) {
-    return console.log('Error reading file:', err);
-  };
+  // Read a file
+  fs.readFile(path.join(__dirname, '/test', 'test.txt'),
+    'utf-8',
+    (err, data) => {
+    if (err) {
+      return console.log('Error reading file:', err);
+    };
 
-  console.log('File read:');
-  console.log(data);
+    console.log('File read:');
+    console.log(data);
+  });
 });
